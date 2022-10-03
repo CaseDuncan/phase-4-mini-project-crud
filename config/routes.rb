8ee1c862bdf524c1140fc6c4a33b048239fe68d6
CaseDuncan <oluochdancun19@gmail.com>
2022-10-03 15:54:33 +0300

    rails crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :spices, only:[:create, :show]
+  get "/spices", to: "spices#index"
+  patch "/spices/:id", to: "spices#update"
+  delete "/spices/:id", to: "spices#destroy"
 end
